01a28e29278c065c6680f32838d06062fb29c7a0
Fola <folaakingbe@yahoo.com>
2018-12-20 22:34:03 -0500

    display

--- score.js
+++ score.js
@@ -1,10 +1,28 @@
 // Global Variables
 
 var scoreString = [];
+var scoreArray = [];
+var rollNumber = 0;
 
 // Functions
 
 function strike(frameNumber) {
-    var frame = scoreString[frameNumber];
     
 };
+
+function displayScore() {
+    var x = document.getElementById("frame-input").value;
+    frameString = x.split("-");
+    for (var i = 0; i < frameString.length; i++) {
+        scoreString += frameString[i];
+    }
+    // for (var j = 0; j < scoreString.length; j++) {
+
+    // }
+    document.getElementById("score").innerHTML = frameString;
+    document.getElementById("score").innerHTML = scoreString;
+    console.log(scoreString);
+    console.log(frameString);
+    console.log(frameString[0][0]);
+
+}
